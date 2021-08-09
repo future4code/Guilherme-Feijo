193e66c54efd517d2eb543773b37537dce8541d2
gtfeijo <gtfeijo@gmail.com>
2021-08-09 14:58:31 -0300

    exercicio variaveis pronto

---
+++ semana1/variaveis/index.js
@@ -0,0 +1,70 @@
+/*
+EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGOS
+
+EXERCÍCIO 1
+SERÁ IMPRESSO 10 NO PRIMEIRO CONSOLE E SERÁ IMPRESSO 10 5 NO SEGUNDO.
+
+EXERCÍCIO 2
+SERÁ IMPRESSO 10 10 10
+
+EXERCÍCIO 3
+
+MELHOR NOME PARA VARIÁVEL p é horasTrabalhadas
+MELHOR NOME PARA VARIÁVEL t é pagamentoDia
+*/
+
+// EXERCÍCIOS DE ESCRITA DE CÓDIGO
+
+/*
+let nome 
+let idade 
+console.log(typeof nome, idade) //O TIPO UNDEFINED FOI ESCRITO PORQUE NENHUMA DAS DUAS VARIAVEIS TIVERAM VALORES ATRIBUIDOS
+*/
+
+
+// let nome = prompt("Qual o seu nome?")
+// let idade = prompt("Quantos anos você tem?")
+// console.log(typeof nome)
+// console.log(typeof idade) //APESAR DA IDADE SER NÚMERO, ELA, ASSIM COMO O VALOR DA VARIAVEL NOME, É STRING
+// console.log("Olá, ",nome,", ","você tem ",idade," anos")
+
+// const gostaBatata = "Você gosta de batata?"
+// const gostaPepino = "Você gosta de pepino?"
+// const gostaRefri = "Você gosta de refrigerante?"
+
+
+// const respBatata = "Sim"
+// const respPepino = "Não"
+// const respRefri = "Sim"
+
+// console.log(gostaBatata, "- ", respBatata)
+// console.log(gostaPepino, "- ", respPepino)
+// console.log(gostaRefri, "- ", respRefri)
+
+// let a = 1
+// let b = 2
+// let c
+
+// c = a
+// a = b
+// b = c
+
+// console.log("O novo valor de a é", a)
+// console.log("O novo valor de b é", b)
+
+
+//DESAFIO
+
+
+
+const numero1s = prompt("Quanto é 1+1?")
+const numero2s = prompt("Quanto é 2+2?")
+
+const numero1n = Number(numero1s)
+const numero2n = Number(numero2s)
+
+const soma = numero1n + numero2n
+const multiplicacao = numero1n * numero2n
+
+console.log("O primeiro número somado ao segundo número resulta em: ", soma)
+console.log("O primeiro número multiplicado pelo segundo número resulta em: ", multiplicacao)
