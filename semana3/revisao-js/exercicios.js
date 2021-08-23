f8b0fb7b88db614a01824f48e893ad5bcad499d4
gtfeijo <gtfeijo@gmail.com>
2021-08-23 14:05:53 -0300

    exercicio 8 feito

--- semana3/revisao-js/exercicios.js
+++ semana3/revisao-js/exercicios.js
@@ -71,8 +71,15 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+   let array = []
+   for (let i = 0; array.length < n; i++){
+       if(i % 2 === 0){
+           array.push(i)
+       }
+   }
+   return array
 }
+
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
