977c18d9cb78ce2f8629d93abb0c77b46c474027
gtfeijo <gtfeijo@gmail.com>
2022-01-27 13:53:03 -0300

    exercicios feitos

---
+++ semana23/aula71/exercicios.ts
@@ -0,0 +1,47 @@
+//EXERCICIO 1
+
+//a)
+
+const printNumbers = (n: number) => {
+  if (n >= 0) {
+    printNumbers(n - 1);
+    console.log(n);
+  }
+};
+
+//b)
+
+const printNumbers = (n: number) => {
+  if (n >= 0) {
+    console.log(n);
+    printNumbers(n - 1);
+  }
+};
+
+//EXERCICIO 2
+
+export const calculateSumTo = (n: number, acc: number = 0): number => {
+  if (n === 0) {
+    return acc;
+  }
+  return calculateSumTo(n - 1, acc + n);
+};
+
+//EXERCICIO 3
+
+export const calculateSumTo = (n: number): number => {
+  let sum = 0;
+  for (var i = 0; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+};
+
+//EXERCICIO 4
+
+export const printArray = (arr: number[], i: number = arr.length - 1) => {
+  if (i >= 0) {
+    printArray(arr, i - 1);
+    console.log(`Elemento ${i}: `, arr[i]);
+  }
+};
